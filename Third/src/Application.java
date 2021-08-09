ebec7cd91676b297172f13ae4b49e2261443e073
John Ferretti <67664301+johnjferretti@users.noreply.github.com>
2021-08-09 13:25:13 -0700

    Add files via upload

---
+++ Third/src/Application.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Application {
+	
+	public static void main(String[] args) throws IOException, JSONException {
+		
+		URL url = new URL("https://swapi.dev/api/people/");
+		
+		System.out.println("Verifying people end point response = 200...");
+		try {
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			System.out.println("Response Code is: " + conn.getResponseCode());
+			assertTrue(conn.getResponseCode() == 200, "Error! Invalid response code.");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		
+		
+		URL url2 = new URL("https://swapi.dev/api/people/");
+		System.out.println("Verifying people end point response = 200...");
+		try {
+			HttpURLConnection conn = (HttpURLConnection) url2.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			System.out.println("Response Code is: " + conn.getResponseCode());
+			assertTrue(conn.getResponseCode() == 200, "Error! Invalid response code.");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
